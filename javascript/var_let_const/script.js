a03465538cd1d61ac5425ce69ca81dec9fd624d1
MijuKim <alwn8918@gmail.com>
2024-09-07 02:25:17 +0900

    JavaScript: var, let, const

---
+++ javascript/var_let_const/script.js
@@ -0,0 +1,29 @@
+// var: 중복 선언 가능, 재할당 가능
+var greeting = "hello";
+console.log(greeting);
+
+var greeting = "hi";
+console.log(greeting);
+
+greeting = "how are you?";
+console.log(greeting);
+
+// let: 중복 선언 불가능, 재할당 가능
+let greeting2 = "hello";
+console.log(greeting2);
+
+// let greeting2 = "hi";
+// console.log(greeting2);
+
+greeting2 = "how are you?";
+console.log(greeting2);
+
+// const: 중복 선언 불가능, 재할당 불가능
+const greeting3 = "hello";
+console.log(greeting3);
+
+// const greeting3 = "hi";
+// console.log(greeting3);
+
+// greeting3 = "how are you?";
+// console.log(greeting3);
