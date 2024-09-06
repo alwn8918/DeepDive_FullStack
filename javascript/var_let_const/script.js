5c6d01a7dbf12c2c886ff99d704523fbb9e80223
MijuKim <alwn8918@gmail.com>
2024-09-07 02:37:15 +0900

    JavaScript: Scope

--- javascript/var_let_const/script.js
+++ javascript/var_let_const/script.js
@@ -1,4 +1,4 @@
-// var: 중복 선언 가능, 재할당 가능
+// var: 중복 선언 가능, 재할당 가능, 함수 레벨 스코프
 var greeting = "hello";
 console.log(greeting);
 
@@ -8,7 +8,17 @@ console.log(greeting);
 greeting = "how are you?";
 console.log(greeting);
 
-// let: 중복 선언 불가능, 재할당 가능
+function func1() {
+  if (true) {
+    var a = "a";
+    console.log(a);
+  }
+  console.log(a);
+}
+// console.log(a);
+func1();
+
+// let: 중복 선언 불가능, 재할당 가능, 블록 레벨 스코프
 let greeting2 = "hello";
 console.log(greeting2);
 
@@ -18,7 +28,17 @@ console.log(greeting2);
 greeting2 = "how are you?";
 console.log(greeting2);
 
-// const: 중복 선언 불가능, 재할당 불가능
+function func2() {
+  if (true) {
+    let b = "b";
+    console.log(b);
+  }
+  // console.log(b);
+}
+// console.log(b);
+func2();
+
+// const: 중복 선언 불가능, 재할당 불가능, 블록 레벨 스코프
 const greeting3 = "hello";
 console.log(greeting3);
 
@@ -27,3 +47,13 @@ console.log(greeting3);
 
 // greeting3 = "how are you?";
 // console.log(greeting3);
+
+function func3() {
+  if (true) {
+    const c = "c";
+    console.log(c);
+  }
+  // console.log(c);
+}
+// console.log(c);
+func3();
