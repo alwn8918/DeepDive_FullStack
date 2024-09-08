ecbe6dc272f6686b41e3cc9f749f7ec1de9ba097
MijuKim <alwn8918@gmail.com>
2024-09-08 22:34:54 +0900

    JavaScript: Window 객체

---
+++ javascript/window/script.js
@@ -0,0 +1,18 @@
+let val;
+
+val = window.outerHeight;
+val = window.innerHeight;
+val = window.outerWidth;
+val = window.innerWidth;
+
+val = window.scrollY;
+val = window.scrollX;
+
+val = window.location;
+// window.location.href = "http://google.com/";
+
+// window.history.go(-2);
+
+val = window.navigator;
+
+console.log(val);
