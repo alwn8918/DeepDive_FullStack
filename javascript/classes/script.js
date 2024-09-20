4a89a5f69d0afa68d23d7163d10c98a37be8d63e
MijuKim <alwn8918@gmail.com>
2024-09-21 03:19:15 +0900

    JavaScript: Sub Class

--- javascript/classes/script.js
+++ javascript/classes/script.js
@@ -1,19 +1,42 @@
+// class Person {
+//   constructor(name, email, birthday) {
+//     this.name = name;
+//     this.email = email;
+//     this.birthday = new Date(birthday);
+//   }
+
+//   introduce() {
+//     return `Hello my name is ${this.name}`;
+//   }
+
+//   // this.name과 같은 것을 사용하지 않는 독립적인 것을 정의할 때 사용
+//   static multipleNumbers(x, y) {
+//     return x * y;
+//   }
+// }
+
+// const john = new Person("john", "john@example.com", "7-10-91");
+// console.log(john);
+
 class Person {
-  constructor(name, email, birthday) {
+  constructor(name, email) {
     this.name = name;
     this.email = email;
-    this.birthday = new Date(birthday);
   }
 
   introduce() {
     return `Hello my name is ${this.name}`;
   }
+}
 
-  // this.name과 같은 것을 사용하지 않는 독립적인 것을 정의할 때 사용
-  static multipleNumbers(x, y) {
-    return x * y;
+class Client extends Person {
+  constructor(name, email, phone, address) {
+    super(name, email);
+
+    this.phone = phone;
+    this.address = address;
   }
 }
 
-const john = new Person("john", "john@example.com", "7-10-91");
-console.log(john);
+const john = new Client("john", "john@example.com", "010-0000-0000", "서울");
+console.log(john.introduce());
